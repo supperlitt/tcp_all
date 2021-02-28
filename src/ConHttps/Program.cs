0da670c2c810a17082869e269f4cd7ac6398816a
超级_小小 <wojiaotanghao@qq.com>
2021-02-28 23:39:52 +0800

    1
    1

---
+++ src/ConHttps/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ConHttps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(new IPEndPoint(IPAddress.Parse("118.31.180.41"), 443));
+
+            NetworkStream ns = new NetworkStream(client, true);
+            SslStream ss = new SslStream(ns, false, new RemoteCertificateValidationCallback(CallBack));
+            ss.AuthenticateAsClient("www.cnblogs.com", new X509CertificateCollection(), System.Security.Authentication.SslProtocols.Tls12, false);
+            ss.Write(Encoding.UTF8.GetBytes(@"GET / HTTP/1.1
+Accept: text/html, application/xhtml+xml, */*
+User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko
+Accept-Encoding: deflate
+Host: www.cnblogs.com
+Connection: Keep-Alive
+Accept-Language: zh-CN
+
+"));
+            ss.Flush();
+
+            byte[] buffer = new byte[8192];
+            int count = ss.Read(buffer, 0, buffer.Length);
+            if (count > 0)
+            {
+                string str = Encoding.UTF8.GetString(buffer, 0, count);
+            }
+        }
+
+        static bool CallBack(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/ConTCPCapture/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ConTCPCapture
+{
+    class Program
+    {
+        private static Queue<byte[]> dataList = new Queue<byte[]>();
+        private static Socket sock = null;
+
+        static void Main(string[] args)
+        {
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
+            sock.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.103"), 0));
+            sock.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+            sock.IOControl(IOControlCode.ReceiveAll, new byte[] { 1, 0, 0, 0 }, new byte[4]);
+
+            Thread t = new Thread(Execute);
+            t.IsBackground = true;
+            t.Start();
+
+            Thread t1 = new Thread(HandleExecute);
+            t1.IsBackground = true;
+            t1.Start();
+
+            Console.WriteLine("Start..");
+            Console.ReadLine();
+        }
+
+        private static void Execute()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[8192];
+                    int count = sock.Receive(buffer);
+                    if (count > 0)
+                    {
+                        int header_len = (buffer[0] & 0xf) * 4;
+                        if (buffer[9] == (int)ProtocolType.Tcp)
+                        {
+                            if (count > header_len)
+                            {
+                                byte[] tcp_data = new byte[count - header_len];
+                                Array.Copy(buffer, header_len, tcp_data, 0, tcp_data.Length);
+
+                                dataList.Enqueue(tcp_data);
+                            }
+                        }
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private static void HandleExecute()
+        {
+            while (true)
+            {
+                try
+                {
+                    var tcp_data = dataList.Dequeue();
+                    if (tcp_data != null)
+                    {
+                        int src_port = ((int)tcp_data[0] << 8) + (int)tcp_data[1];
+                        int target_port = ((int)tcp_data[2] << 8) + (int)tcp_data[3];
+                        if (src_port == 80 || target_port == 80)
+                        {
+                            int header_len = (tcp_data[12] & 0xf) * 4;
+                            if (tcp_data.Length > header_len)
+                            {
+                                byte[] http_data = new byte[tcp_data.Length - header_len];
+                                Array.Copy(tcp_data, header_len, http_data, 0, http_data.Length);
+
+                                Console.WriteLine(Encoding.UTF8.GetString(http_data));
+                            }
+                        }
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+}
